319bd2b3643276566c7fb25c48c82a77eaadbf86
Namit Nathwani <namsnath@gmail.com>
2018-10-23 23:19:29 +0530

    Added error function

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const Promise = require("bluebird");
 const app = express();
 const port = 3000;
 
+app.use(function (err, req, res, next) {
+  	//res.status(err.status || 500);
+  	if(err.status = 400)
+  	{
+  		console.log(err);
+  		res.redirect("/");
+  	}
+  	else
+  		res.sendStatus(err.status)
+});
+
 app.listen(port, () => {
 	console.log("Server listening on port " + port)
 });
